3bc11c450ec38ca4f24fa89ef1080012714df6a9
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-12-27 01:48:12 -0500

    chore: CI modifications (#25)
    * chore: change default ci block
    
    * fix: rustflags during build and +avx512f
    
    * fix: bad script

--- run.sh
+++ run.sh
@@ -14,4 +14,20 @@ cd ../..
 mkdir -p rpc-cache
 source .env
 MODE=execute # can be execute, prove, or prove-e2e
-RUSTFLAGS="-Ctarget-cpu=native" RUST_BACKTRACE=1 OUTPUT_PATH="metrics.json" cargo run --bin openvm-reth-benchmark --release -- --$MODE --block-number 18884864 --rpc-url $RPC_1 --cache-dir rpc-cache
+PROFILE="release"
+
+arch=$(uname -m)
+case $arch in
+arm64|aarch64)
+    RUSTFLAGS="-Ctarget-cpu=native"
+    ;;
+x86_64|amd64)
+    RUSTFLAGS="-Ctarget-cpu=native -C target-feature=+avx512f"
+    ;;
+*)
+echo "Unsupported architecture: $arch"
+exit 1
+;;
+esac
+RUSTFLAGS=$RUSTFLAGS cargo build --bin openvm-reth-benchmark --profile=$PROFILE --no-default-features --features=$FEATURES
+RUST_BACKTRACE=1 OUTPUT_PATH="metrics.json" ./target/$PROFILE/openvm-reth-benchmark --$MODE --block-number 18884864 --rpc-url $RPC_1 --cache-dir rpc-cache
