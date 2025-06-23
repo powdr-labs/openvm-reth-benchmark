bf5a014ec91e180a5de32feceea6cff435bd2c26
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-06-23 07:06:07 -0700

    chore: fix imports (#431)
    * fix: stark-sdk import
    
    * update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ mkdir -p rpc-cache
 source .env
 MODE=execute # can be execute, tracegen, prove-app, prove-stark, or prove-evm
 PROFILE="release"
-FEATURES="bench-metrics,nightly-features,jemalloc"
+FEATURES="bench-metrics,nightly-features,jemalloc,evm-verify"
 BLOCK_NUMBER=21882667
 
 arch=$(uname -m)
