e43635196690faed62cb2a1fce97493d7b9f2e48
Leo Alt <leo@powdrlabs.com>
2025-09-08 17:57:14 +0200

    compiles with no manual precompiles

--- bin/client-eth/src/main.rs
+++ bin/client-eth/src/main.rs
@@ -2,13 +2,13 @@ use openvm::io::{println, read, reveal_bytes32};
 use openvm_client_executor::{io::ClientExecutorInput, ClientExecutor};
 // Imports needed by the linker, but clippy can't tell:
 #[allow(unused_imports, clippy::single_component_path_imports)]
-use {
-    k256::Secp256k1Point,
-    openvm_algebra_guest::IntMod,
-    openvm_pairing::{bls12_381::Bls12_381G1Affine, bn254::Bn254G1Affine},
-};
+// use {
+//     k256::Secp256k1Point,
+//     openvm_algebra_guest::IntMod,
+//     openvm_pairing::{bls12_381::Bls12_381G1Affine, bn254::Bn254G1Affine},
+// };
 
-openvm::init!();
+// openvm::init!();
 
 pub fn main() {
     println("client-eth starting");
