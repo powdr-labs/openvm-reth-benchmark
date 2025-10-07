df5341af7ba7d17e0456f79ed3aceff1eda98f41
Lucas Clemente Vella <lvella@powdrlabs.com>
2025-10-07 14:43:52 -0300

    Not sure what builds, but it builds.

--- bin/client-eth/src/main.rs
+++ bin/client-eth/src/main.rs
@@ -1,5 +1,7 @@
 use openvm::io::{println, read, reveal_bytes32};
 use openvm_client_executor::{io::ClientExecutorInput, ClientExecutor};
+use getrandom::{register_custom_getrandom, Error};
+
 // Imports needed by the linker, but clippy can't tell:
 #[allow(unused_imports, clippy::single_component_path_imports)]
 // use {
@@ -24,3 +26,12 @@ pub fn main() {
     // Reveal the block hash.
     // reveal_bytes32(*block_hash);
 }
+
+// Implementation taken from here: https://xkcd.com/221/
+fn random_source(buf: &mut [u8]) -> Result<(), Error> {
+    for byte in buf.iter_mut() {
+        *byte = 4; // Chosen by fair dice roll. Guaranteed to be random.
+    }
+    Ok(())
+}
+register_custom_getrandom!(random_source);
