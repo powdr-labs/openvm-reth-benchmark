95d6cd199e1730a533f782025f8db41a104f8b66
Leo Alt <leo@powdrlabs.com>
2025-10-08 14:52:31 +0200

    wasm env

--- bin/client-eth/src/main.rs
+++ bin/client-eth/src/main.rs
@@ -1,6 +1,21 @@
-use openvm::io::{println, read, reveal_bytes32};
+// use openvm::io::{println, read, reveal_bytes32};
 use openvm_client_executor::{io::ClientExecutorInput, ClientExecutor};
 use getrandom::{register_custom_getrandom, Error};
+
+#[link(wasm_import_module = "env")]
+unsafe extern "C" {
+    pub safe fn read_u32() -> u32;
+    pub safe fn read() -> &'static [u8];
+    pub safe fn println(_: &str);
+}
+
+use serde::de::DeserializeOwned;
+use postcard;
+
+fn read_t<T: DeserializeOwned>() -> T {
+    let bytes = read();
+    postcard::from_bytes(&bytes).unwrap()
+}
 
 // Imports needed by the linker, but clippy can't tell:
 #[allow(unused_imports, clippy::single_component_path_imports)]
@@ -15,7 +30,8 @@ use getrandom::{register_custom_getrandom, Error};
 pub fn main() {
     println("client-eth starting");
     // Read the input.
-    let input: ClientExecutorInput = read();
+    // TODO check how tihs was serialized.
+    let input: ClientExecutorInput = read_t();
     println("finished reading input");
 
     // Execute the block.
