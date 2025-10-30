667ff5bbc099b880ce2e6d62661b4fb87a6a2878
qwang98 <qian.wang.wg24@wharton.upenn.edu>
2025-10-30 09:59:43 +0100

    comment out MODE

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ set -e
 
 mkdir -p rpc-cache
 source .env
-MODE=execute # can be execute-host, execute, execute-metered, prove-app, prove-stark, or prove-evm (needs "evm-verify" feature)
+# MODE=execute # can be execute-host, execute, execute-metered, prove-app, prove-stark, or prove-evm (needs "evm-verify" feature)
 
 cd bin/client-eth
 RUSTFLAGS="-Clink-arg=--emit-relocs" cargo openvm build --no-transpile
