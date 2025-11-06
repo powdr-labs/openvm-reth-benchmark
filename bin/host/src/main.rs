a7d9fd75eee57c405548187f68a76856ce789da3
Leandro Pacheco <contact@leandropacheco.com>
2025-11-06 18:44:48 -0300

    more prints

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -9,6 +9,14 @@ const OPENVM_CLIENT_ETH_ELF: &[u8] = include_bytes!("../elf/openvm-client-eth");
 async fn main() -> eyre::Result<()> {
     let args = HostArgs::parse();
     let args = complete_args(args);
+    let start = std::time::Instant::now();
     let setup = precompute_prover_data(&args, OPENVM_CLIENT_ETH_ELF).await?;
-    run_reth_benchmark(args, setup, OPENVM_CLIENT_ETH_ELF).await
+    let elapsed = start.elapsed();
+    println!(">>>>>>> PRECOMPUTE_PROVER_DATA: {:?}", elapsed);
+
+    let start = std::time::Instant::now();
+    let res = run_reth_benchmark(args, setup, OPENVM_CLIENT_ETH_ELF).await;
+    let elapsed = start.elapsed();
+    println!(">>>>>>> RUN_RETH_BENCHMARK: {:?}", elapsed);
+    res
 }
